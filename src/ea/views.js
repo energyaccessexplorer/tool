7731f73fb71ff61c9da36e1d26ffc7cdbdea8517
Fabian <eae@carajo.no>
2018-08-06 09:25:31 +0200

    moved views to layers and added view.active

--- src/ea/views.js
+++ src/ea/views.js
@@ -10,12 +10,18 @@ function ea_views_init() {
     const btn = elem(`<div class="view">${views[t]}</div>`);
 
     btn.addEventListener('mouseup', function(e) {
+      el.querySelectorAll('.view').forEach(e => e.classList.remove('active'));
+
+      btn.classList.add('active');
+
       ea_overlord({
         type: "mode",
         target: t,
         caller: "ea_views_init",
       });
     });
+
+    if (location.get_query_param('mode') === t) btn.classList.add('active');
 
     el.appendChild(btn);
   })

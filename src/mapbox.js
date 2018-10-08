3be4287839866b2284395af821eb2c0c959fcbd4
Fabian <eae@carajo.no>
2018-10-08 11:52:24 +0200

    mapbox style changer

--- src/mapbox.js
+++ src/mapbox.js
@@ -1,3 +1,76 @@
+class MapboxThemeControl {
+  onAdd(map) {
+    this._map = map;
+    this._container = document.createElement('div');
+    this._container.className = 'mapboxgl-ctrl';
+    this._container.classList.add('mapboxgl-ctrl-group');
+
+    let button = elem(`
+<button class="mapboxgl-ctrl-icon" type="button">
+<div style="transform: scale(0.75);">
+<svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z"/></svg>
+<div>
+</button>
+`)
+
+    this._container.appendChild(button);
+
+    button.addEventListener('mouseup', function() {
+      let content = elem('<div>');
+
+      let radios = elem(`<div>
+<h3>Background map style</h3>
+
+<input type="radio" name="mapbox_theme" value="dark" /> <label>Dark (default)</label> <br>
+<input type="radio" name="mapbox_theme" value="basic" /> <label>Basic</label> <br>
+<input type="radio" name="mapbox_theme" value="satellite" /> <label>Satellite</label> <br>
+<input type="radio" name="mapbox_theme" value="streets" /> <label>Streets</label> <br>
+<input type="radio" name="mapbox_theme" value="satellite-streets" /> <label>Satellite Streets</label> <br>
+</div>`);
+
+      radios.querySelector(`input[value="${ea_settings.mapbox_theme}"]`).setAttribute('checked', true);
+
+      radios.querySelectorAll('input[name="mapbox_theme"]').forEach(e => {
+        e.addEventListener('change', _ => {
+          ea_mapbox.setStyle('mapbox://styles/mapbox/' + e.value + '-v9');
+          mapbox_canvas(ea_mapbox);
+        });
+      });
+
+      content.appendChild(radios);
+
+      modal()
+        .header("Map configuration")
+        .content(content)();
+    });
+
+    return this._container;
+  }
+
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
+};
+
+function mapbox_canvas(m, coords) {
+  m.addSource('canvas-source', {
+    type: 'canvas',
+    canvas: 'plot',
+    coordinates: coords
+  });
+
+  const c = m.getStyle().layers.find(l => l.type === 'symbol')
+
+  ea_mapbox.first_symbol = ((c && c.id) || undefined);
+
+  m.addLayer({
+    id: 'canvas-layer',
+    source: 'canvas-source',
+    type: 'raster',
+  }, ea_mapbox.first_symbol);
+}
+
 function mapbox_setup(bounds, theme, token) {
   mapboxgl.accessToken = token;
 
@@ -8,7 +81,7 @@ function mapbox_setup(bounds, theme, token) {
 
   mapbox.fitBounds(bounds, { animate: false });
 
-  mapbox.on('load', _ => {
+  mapbox.on('style.load', _ => {
     const b = bounds;
 
     const r = b[0][0];
@@ -23,22 +96,12 @@ function mapbox_setup(bounds, theme, token) {
       [r, d]
     ];
 
-    mapbox.addSource('canvas-source', {
-      type: 'canvas',
-      canvas: 'plot',
-      coordinates: coords
-    });
-
-    ea_mapbox.first_symbol = mapbox.getStyle().layers.find(l => l.type === 'symbol').id;
-
-    mapbox.addLayer({
-      id: 'canvas-layer',
-      source: 'canvas-source',
-      type: 'raster',
-    }, ea_mapbox.first_symbol);
+    mapbox_canvas(mapbox, coords);
   });
 
   mapbox.addControl(new mapboxgl.NavigationControl({ showCompass: false }));
+
+  mapbox.addControl((new MapboxThemeControl()), 'top-right');
 
   return mapbox;
 };

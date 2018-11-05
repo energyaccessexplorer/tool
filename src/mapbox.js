8de8f433e787e9acfb5529c3d76169cd57da527a
Fabian <eae@carajo.no>
2018-11-05 23:53:29 +0100

    minor style changes and cleanups

--- src/mapbox.js
+++ src/mapbox.js
@@ -24,11 +24,11 @@ class MapboxThemeControl {
 
     let button = elem(`
 <button class="mapboxgl-ctrl-icon" type="button">
-<div style="transform: scale(0.75);">
-<svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z"/></svg>
-<div>
+  <div style="transform: scale(0.75);">
+    <svg width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z"/></svg>
+  <div>
 </button>
-`)
+`);
 
     this._container.appendChild(button);
 

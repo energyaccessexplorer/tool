dd4d20fed3db29029360eee0bb1eb7ab6ece5104
Fabian <eae@carajo.no>
2018-12-20 07:43:42 +0800

    dropped views.js

--- src/ea/auxiliary.js
+++ src/ea/auxiliary.js
@@ -152,3 +152,27 @@ function ea_summary() {
 
   return summary;
 };
+
+function ea_views_init() {
+  const el = document.querySelector('#views');
+
+  Object.keys(ea_views).forEach(v => {
+    const btn = elem(`<div class="view">${ea_views[v]}</div>`);
+
+    btn.addEventListener('mouseup', function(e) {
+      el.querySelectorAll('.view').forEach(e => e.classList.remove('active'));
+
+      btn.classList.add('active');
+
+      ea_overlord({
+        "type": "mode",
+        "target": v,
+        "caller": "ea_views_init",
+      });
+    });
+
+    if (location.get_query_param('mode') === v) btn.classList.add('active');
+
+    el.appendChild(btn);
+  })
+};

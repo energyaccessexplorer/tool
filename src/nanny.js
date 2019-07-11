696492c86b0f0d5f289694259d8c15eecadfb60f
Fabian <eae@carajo.no>
2019-07-11 18:30:01 +0200

    nanny revisited

--- src/nanny.js
+++ src/nanny.js
@@ -11,20 +11,18 @@ function ea_nanny_el_wait_action(el, action, callback) {
 };
 
 function ea_nanny_pick_element(query, opts) {
-  let x,y;
   const el = document.querySelector(query);
 
   if (!el) throw DOMError, `ea_nanny_pick_element: No such element found with '${query}'`;
 
-  y = el.offsetTop;
-  x = el.offsetLeft;
+  const elbox = el.getBoundingClientRect();
 
   let body;
 
   if (opts.message instanceof HTMLElement)
     body = opts.message;
   else
-    body = elem(opts.message, 'p');
+    body = elem(opts.message || "", 'p');
 
   const marker = elem(`
 <aside class="nanny-marker">
@@ -36,36 +34,116 @@ function ea_nanny_pick_element(query, opts) {
   marker.appendChild(body);
   document.body.appendChild(marker);
 
-  let orient, numx, numy;
-  orient = opts.orient.match(/(.*)\ (.*)/);
+  const caret = marker.querySelector('.nanny-caret');
 
-  if (orient) {
-    numx = parseInt(orient[1].match(/([0-9]+)/));
-    numy = parseInt(orient[2].match(/([0-9]+)/));
+  let x = elbox.x;
+  let y = elbox.y;
+
+  function halign() {
+    switch (opts.align) {
+    case "start":
+      x += 0;
+      break;
+
+    case "end":
+      x += elbox.width - marker.clientWidth;
+      break;
+
+    case "middle":
+    default:
+      x += (elbox.width / 2) - (marker.clientWidth / 2);
+      break;
+    }
+  };
+
+  function valign() {
+    switch (opts.align) {
+    case "start":
+      y -= marker.clientHeight / 2;
+      break;
+
+    case "end":
+      y += elbox.height - (marker.clientHeight / 2);
+      break;
+
+    case "middle":
+    default:
+      y = elbox.y + (elbox.height / 2) - (marker.clientHeight / 2);
+      break;
+    }
+  };
+
+  const caretsize = 100;
+  const bc = "rgba(0,0,0,1)";
+
+  switch (opts.position) {
+  case "N":
+  case "north": {
+    y -= marker.clientHeight;
+    halign();
+
+    caret.style['border-width'] = (marker.clientWidth / 2) + "px";
+    caret.style['border-top-color'] = bc;
+    caret.style['transform'] = `scale(1, 0.5)`;
+    caret.style['top'] = (marker.clientHeight - (marker.clientWidth / 4) - 0.5) + "px";
+
+    break;
   }
 
-  let l,t;
+  case "E":
+  case "east": {
+    x += elbox.width;
+    valign();
 
-  if (orient[1] == "right")
-    l = x + el.offsetWidth;
-  else if (orient[1] == "center")
-    l = (x + (el.offsetWidth / 2) - (marker.clientWidth / 2));
-  else if (!!numx)
-    l = x + parseInt(numx);
-  else
-    l = x;
+    caret.style['border-width'] = (marker.clientHeight / 2) + "px";
+    caret.style['border-right-color'] = bc;
+    caret.style['transform'] = `scale(0.5, 1)`;
+    caret.style['left'] = -((marker.clientHeight * (3/4)) - 0.5) + "px";
 
-  if (orient[2] == "bottom")
-    t = (y + el.offsetHeight - marker.clientHeight);
-  else if (orient[2] == "middle")
-    t = (y + (el.offsetHeight / 2) - (marker.clientHeight / 2));
-  else if (!!numy)
-    t = y + parseInt(numy);
-  else
-    t = y;
+    break;
+  }
 
-  marker.style.left = l + "px"
-  marker.style.top = t + "px"
+  case "S":
+  case "south": {
+    y = elbox.y + marker.clientHeight;
+    halign();
+
+    caret.style['border-width'] = (marker.clientWidth / 2) + "px";
+    caret.style['border-bottom-color'] = bc;
+    caret.style['transform'] = `scale(1, 0.25)`;
+    caret.style['top'] = -((marker.clientWidth * (5/8)) - 0.25) + "px";
+
+    break;
+  }
+
+  case "W":
+  case "west": {
+    x -= marker.clientWidth;
+    valign();
+
+    caret.style['border-width'] = (marker.clientHeight / 2) + "px";
+    caret.style['border-left-color'] = bc;
+    caret.style['transform'] = `scale(0.5, 1)`;
+    caret.style['left'] = marker.clientWidth - ((marker.clientHeight * (1/4)) + 0.5) + "px";
+
+    break;
+  }
+
+  case "C":
+  case "CENTER": {
+    caret.style['display'] = 'none';
+    opts.align = 'middle';
+    valign();
+    halign();
+    break;
+  }
+
+  default:
+    break;
+  }
+
+  marker.style.left = x + "px"
+  marker.style.top = y + "px"
 
   return marker;
 };
@@ -83,15 +161,17 @@ function ea_nanny_next() {
 function ea_nanny_start() {
   if (typeof ea_nanny_steps === 'undefined') return;
 
-  if (ea_state_sync().inputs.length > 1) return;
+  const s = ea_state_sync();
+
+  if (s.inputs.length > 1) return;
+  if (s.mode !== "inputs") return;
 
   const w = localStorage.getItem('needs-nanny');
 
   if (!w || !w.match(/false/)) {
-    ea_nanny_steps[0]();
-    ea_modal.show();
+    setTimeout(_ => ea_nanny_steps[(ea_nanny_current_step = 0)](), 1000)
   }
-};
+}
 
 function ea_nanny_finish() {
   console.log("Nanny's done.");

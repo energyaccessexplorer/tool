334fcaaf9f5c3de739611946843834ab50dd3a99
Fabian <eae@carajo.no>
2019-07-14 11:35:07 +0200

    consider heatmap factor when clicking on map

--- src/ea.js
+++ src/ea.js
@@ -431,11 +431,13 @@ Please report this to energyaccessexplorer@wri.org.
       );
 
       if (rc && rc.value) {
+        v = rc.value * t.heatmap.factor;
+
         table_pointer([{
           "target": t.name_long,
           "dataset": "value"
         }], {
-          "value": `${(rc.value).toFixed(2)} <code>${t.unit || ''}</code>`
+          "value": `${v.toFixed(2)} <code>${t.unit || ''}</code>`
         }, e);
       }
       else {

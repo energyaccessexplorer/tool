3ed767fd1bd9950a3c5fcb16d084a49e5946d5d2
Fabian <eae@carajo.no>
2021-06-04 10:46:53 +0200

    super fixie

--- src/a.js
+++ src/a.js
@@ -354,7 +354,7 @@ async function dsinit(id, inputs, pack, callback) {
 	let select = ["*", "category:categories(*)", "df:_datasets_files(*,file:files(*))"];
 
 	let bounds;
-	const boundaries_id = maybe(GEOGRAPHY.configuration, 'boundaries', 0, 'dataset_id');
+	const boundaries_id = maybe(GEOGRAPHY.configuration, 'divisions', 1, 'dataset_id');
 
 	if (!boundaries_id) {
 		const m = `

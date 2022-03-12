1950007b357bff3abbf660931511a14dc800b913
Fabian <fabian@energyaccessexplorer.org>
2022-03-12 18:16:58 +0100

    analysis-search: user-provided csv file

--- src/analysis-search.js
+++ src/analysis-search.js
@@ -11,9 +11,7 @@ let ul, input, resultscontainer;
 
 let resultsinfo;
 
-const n = 20;
-
-async function getpoints() {
+async function getpoints(n = 20) {
 	const a = await analysis_plot_active(U.output, false);
 
 	const threshold = a.raster.slice(0)
@@ -38,7 +36,7 @@ function pointto(p, a = false) {
 };
 
 function li(p) {
-	const c = Math.round((p.v).toFixed(2) * 100) + " " + JSON.stringify((p.i).map(c => +c.toFixed(3)));
+	const c = (p.v ? Math.round((p.v).toFixed(2) * 100) : "") + " " + "[" + (p.i).map(c => +c.toFixed(3)).join(", ") + "]";
 
 	const el = ce('li', ce('code', c), {});
 
@@ -49,10 +47,10 @@ function li(p) {
 	return el;
 };
 
-async function trigger() {
+async function trigger({ points = getpoints, n = 20 }) {
 	ul.replaceChildren();
 
-	const results = await getpoints();
+	const results = await points(n);
 
 	const count = results.length;
 
@@ -69,12 +67,65 @@ async function trigger() {
 
 export function init() {
 	const panel = qs('#analysis.search-panel');
-	input = ce('span', "Analysis top locations",
-						 { id: 'analysis-search', class: 'search-input' });
+	input = ce('span', "Analysis top locations", { id: 'analysis-search', class: 'search-input' });
+
+	const file_input = ce('input', null, { type: "file", id: "location-search-input", style: "width: 0; height: 0;", accept: "text/csv" });
+	const upload_icon = font_icon("box-arrow-in-up");
+	const upload = ce('label', upload_icon, { class: "search-input", for: "location-search-input" });
+
+	const msg = `
+<h1>Upload a CSV file</h1>
+
+This file should be <strong>strictly</strong> formatted.
+<ul>
+  <li>Two numbers per line: longitud and latitude</li>
+  <li>Separated by a comma</li>
+  <li>Decimal points</li>
+  <li>No quotation marks</li>
+  <li>Nothing more</li>
+</ul>
+
+<p>Example with two coordinates:</p>
+<pre>
+    -12.09156,9.05680
+    -10.90457,7.93582
+</pre>
+`;
+
+	let bubble;
+
+	upload.onmouseenter = _ => {
+		bubble = bubblearrow(upload_icon, {
+			position: "E",
+			message: msg,
+			close: false
+		});
+
+		bubble.style['pointer-events'] = "none";
+	};
+
+	upload.onmouseleave = _ => bubble.remove();
+
+	file_input.onchange = function() {
+		if (!this.files.length) return;
+
+		var reader = new FileReader();
+
+		reader.onload = function() {
+			const data = d3.csvParseRows(reader.result, d => ({
+				v: null,
+				i: [parseFloat(d[0]), parseFloat(d[1])],
+			}));
+
+			trigger({ points: async _ => data });
+		};
+
+		reader.readAsText(this.files[0]);
+	};
 
 	panel.addEventListener('activate', trigger);
 
-	panel.prepend(input);
+	panel.prepend(input, file_input, upload);
 
 	resultscontainer = qs('#analysis .search-results');
 	ul = ce('ul');

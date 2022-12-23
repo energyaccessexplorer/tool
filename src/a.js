a56d3b79c05bc4fc14289137e526a79e84429c66
Fabian <fabian@energyaccessexplorer.org>
2022-12-23 20:29:37 +0100

    rewrote geographies-search to use a proper tree structure

--- src/a.js
+++ src/a.js
@@ -303,6 +303,36 @@ This is fatal. Thanks for all the fish.`;
 
 	ALL.filter(d => !divisions.map(i => i.dataset_id).includes(d.id)).map(e => new DS(e));
 
+	(async function fetch_admintiers() {
+		const ds = new DS(ALL.find(x => x.category.name === 'admin-tiers'));
+		await ds.load('csv');
+
+		const data = ds.csv.data;
+		const columns = data.columns;
+
+		const tree = [];
+
+		function insert(nums, t) {
+			const head = maybe(nums, 0);
+
+			if (nums.length === 1) {
+				t[head] = 1;
+				return t;
+			}
+
+			if (!nums.length) return t;
+
+			if (t[head] === undefined) t[head] = [];
+
+			return insert(nums.slice(1), t[head]);
+		};
+
+		for (let i = 0; i < data.length; i++)
+			insert(columns.map(c => data[i][c]), tree);
+
+		ds.tree = tree;
+	})();
+
 	// We need all the datasets to be initialised _before_ setting
 	// mutant attributes (order is never guaranteed)
 	//
--- src/geographies-search.js
+++ src/geographies-search.js
@@ -2,139 +2,41 @@ import {
 	fit as mapbox_fit,
 } from './mapbox.js';
 
-let ul, input, resultscontainer;
-
-const lists = [];
-let geometry_path = [];
-let loaded_list = 1;
-
-const resultsinfo = ce('div', null, {
-	"class": 'search-results-info',
-	"style": "cursor: pointer;",
-});
-
-function div(g,i,j) {
-	const el = tmpl('#geographies-search-item');
-	bind(el, {
-		"name": g.name,
-		"url":  g.id ? `./?id=${g.id}` : null,
-	});
-
-	qs('[zoom]', el).onclick = load.bind(null, i, j);
-
-	return ce('li', el);
-};
-
-function li(g,i,j) {
-	const el = ce('li', g.name);
-	el.onclick = load.bind(null,i,j);
-
-	return el;
-};
+let input, resultscontainer;
+let all = [];
+let details = [];
 
 function trigger(value) {
-	if (!lists[U.divtier + 1]) return;
+	details.forEach(x => x.removeAttribute('open'));
 
-	for (let i of lists[U.divtier + 1])
-		i.li.style.display = i.name.match(new RegExp(value, 'i')) ? "" : "none";
-};
+	const re = new RegExp(value, 'i');
+	if (String(re) === "/(?:)/i") {
+		all.forEach(x => x.classList.remove('matches'));
+		details[0]?.setAttribute('open', '');
+		return;
+	}
 
-function fetch_countries() {
-	const p = {
-		"select":         ["id", "name"],
-		"datasets_count": "gt.0",
-		"parent_id":      GEOGRAPHY.parent_id ? `eq.${GEOGRAPHY.parent_id}` : "is.null",
-		"adm":            `eq.0`,
-		"deployment":     `ov.{${ENV}}`,
-		"order":          "name.asc",
-	};
+	all.forEach(x => {
+		x.classList.remove('matches');
 
-	return API.get("geographies", p).then(j => {
-		return j.map((g,i) => {
-			const li = ce('li', g.name);
+		if (x.textContent.match(re))
+			x.classList.add('matches');
+	});
 
-			li.onclick = function() {
-				if (g.id === GEOGRAPHY.id) {
-					load(0,0);
-				} else {
-					const url = new URL(location);
-					url.searchParams.set('id', g.id);
-					location = url;
-				}
-			};
-
-			return {
-				i,
-				li,
-				"name": g.name,
-			};
-		});
+	details.forEach(x => {
+		if (x.querySelector('.matches'))
+			x.setAttribute('open', '');
 	});
 };
 
 async function load(x,y) {
 	U.divtier = x;
 	U.subdiv = y;
-	O.view = U.view;
 
 	const fs = maybe(GEOGRAPHY.divisions, x, 'vectors', 'geojson', 'features');
 	if (!fs) return;
 
 	const geometry = fs.find(f => f['id'] === y);
-
-	geometry_path[x] = y;
-
-	resultsinfo.replaceChildren(
-		font_icon('arrow-up'),
-		ce('span', "Up a level", { "style": "margin-left: 1em;"}),
-	);
-
-	resultsinfo.onclick = function(_) {
-		const x = U.divtier;
-
-		if (loaded_list === 0) return;
-
-		if (x === 0) {
-			ul.replaceChildren(...lists[0].map(x => x.li));
-			loaded_list = 0;
-			resultsinfo.replaceChildren(ce('span', "Top level", { "style": "margin-left: 1em; color: gray;" }));
-			return;
-		}
-
-		const d = GEOGRAPHY.divisions[x];
-		d.vectors.geojson.features.forEach(f => f.properties.__visible = true);
-		MAPBOX.getSource(d.id).setData(DST.get(d.id).vectors.geojson);
-
-		load(x - 1, geometry_path[x - 1]);
-	};
-
-	if (lists[x+1]) {
-		let tiers;
-		const at = DST.get('admin-tiers');
-		if (at) await until(_ => tiers = at.csv.data);
-
-		ul.replaceChildren(...lists[x+1].filter(v => {
-			if (!at) return true;
-
-			const t = tiers.find(r => +r['TIER' + (x+1)] === v.i);
-
-			if (!t) return true;
-
-			const e = t['TIER' + x];
-
-			return (t && e === undefined) ? true : +e === U.subdiv;
-		}).map(i => i.li));
-	} else {
-		resultsinfo.append(ce('span', "(No more subdivisions)", { "style": "margin-left: 1em; color: gray;" }));
-	}
-
-	if (!maybe(lists, x, 'length')) {
-		resultsinfo.replaceChildren(ce('span', "Already at top level", { "style": "margin-left: 1em; color: gray;" }));
-		resultsinfo.onclick = null;
-	}
-
-	loaded_list = x+1;
-
 	if (geometry)
 		mapbox_fit(geojsonExtent(geometry), true);
 
@@ -144,10 +46,7 @@ async function load(x,y) {
 	d.vectors.geojson.features.forEach(f => f.properties.__visible = (f.id === y));
 	MAPBOX.getSource(d.id).setData(DST.get(d.id).vectors.geojson);
 
-	if (d.on) {
-		await delay(1);
-		d.raise();
-	}
+	if (d.on) d.raise();
 };
 
 export async function init() {
@@ -158,48 +57,6 @@ export async function init() {
 	panel.prepend(input);
 
 	resultscontainer = qs('#geographies .search-results');
-	ul = ce('ul');
-	resultscontainer.append(ul);
-
-	const at = DST.get('admin-tiers');
-	if (at) at.load('csv');
-
-	const gid = (new URL(location)).searchParams.get('id');
-
-	await API
-		.get("geographies", {
-			"select":     ["name", "id"],
-			"parent_id":  `eq.${gid}`,
-			"deployment": `ov.{${ENV}}`,
-		})
-		.then(r => {
-			const fli = r.find(e => maybe(e.id)) ? div : li;
-
-			GEOGRAPHY
-				.divisions
-				.forEach((d,i) => {
-					const v = maybe(d, 'csv', 'value');
-					const a = !v ? [] : d.csv.data.map(x => x[v]);
-
-					lists[i] = a.map((g,j) => {
-						const k = +d.csv.data[j][d.csv.key];
-						const id = maybe(r.find(e => e.name === g), 'id');
-
-						const geo = { "name": g, id };
-
-						return {
-							"i":    k,
-							"li":   fli(geo, i, k),
-							"name": g,
-						};
-					});
-				});
-		});
-
-	lists[0] = await fetch_countries();
-
-	resultscontainer.prepend(resultsinfo);
-	load(0,0);
 
 	input.oninput = function(_) {
 		trigger(this.value);
@@ -210,11 +67,46 @@ export async function init() {
 		trigger(this.value);
 	};
 
-	input.onkeypress = function(e) {
-		if (e.key !== 'Enter') return;
+	const d = await tree();
+	d.setAttribute('open', '');
 
-		const c = Array.from(qsa('li', ul)).find(x => x.style.display !== 'none');
+	resultscontainer.replaceChildren(d);
 
-		if (c) c.dispatchEvent(new Event('click'));
+	all = resultscontainer.querySelectorAll('summary,div');
+	details = resultscontainer.querySelectorAll('details');
+
+	load(0,0);
+};
+
+async function tree() {
+	await until(_ => DST.get('admin-tiers').tree);
+
+	const divisions = GEOGRAPHY.divisions;
+
+	function subtree(branch, j, title, y) {
+		const s = ce('summary', title);
+		const d = ce('details', s);
+		s.onclick = load.bind(null, j, y);
+
+		for (let i = 0; i < branch.length; i++) {
+			if (!branch[i])
+				continue;
+
+			else if (branch[i] === 1) {
+				const x = ce('div', divisions[j+1].csv.table[i]);
+				x.onclick = load.bind(null, j+1, i);
+				d.append(x);
+			}
+
+			else
+				d.append(subtree(branch[i], j+1, divisions[j+1].csv.table[i], i));
+		}
+
+		return d;
 	};
+
+	const t = subtree(DST.get('admin-tiers').tree, 0, GEOGRAPHY.name);
+	t.querySelector('summary').onclick = load.bind(null, 0, 0);
+
+	return t;
 };

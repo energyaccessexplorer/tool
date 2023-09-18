e97450b06bac3c228f8edbba379ef23d084ec480
Fabian <eae@263.nu>
2023-09-18 22:10:19 +0200

    /m revisited

--- src/m.js
+++ src/m.js
@@ -4,12 +4,95 @@ import {
 
 import bind from './bind.js';
 
+import modal from '../lib/modal.js';
+
 function unique(arr) {
 	return arr.filter((v,i,a) => a.indexOf(v) === i);
 };
 
 function loading(bool) {
 	qs('#app-loading').style['display'] = bool ? 'block' : 'none';
+};
+
+function filtering(sessions) {
+	const els = Array.from(document.querySelectorAll('.session-square'));
+
+	document.querySelector('input#search').oninput = debounce(function(event) {
+		const v = event.target.value;
+
+		const r = new RegExp(v, "i");
+
+		for (const e of els) {
+			const s = sessions.find(s => s.time == e.getAttribute('data'));
+			if (!s) continue;
+
+			e.style.display = (or(
+				s.geography_names_path.join("|").match(r),
+				s.title?.match(r),
+			)) ? '' : 'none';
+		}
+	}, 300);
+};
+
+function edit_title(sessions) {
+	const div = this.closest('.session-square');
+	const data = div.getAttribute('data');
+
+	const s = sessions.find(s => s.time === +data);
+	if (!s) return;
+
+	const input = document.createElement('input');
+	input.className = "title-input";
+
+	input.oninput = debounce(function() {
+		API.patch('sessions', {
+			"time": `eq.${s.time}`,
+		}, {
+			"payload": {
+				"title": input.value,
+			},
+		}).then(_ => {
+			div.querySelector('.title').innerText = input.value;
+			API.flash.push({ "message": "Title updated", "type": "success" });
+		});
+	}, 300);
+
+	const m = new modal({
+		"header":  "Edit title",
+		"content": input,
+	});
+
+	m.show();
+};
+
+function draw_sessions(sessions, geographies, container, trees) {
+	sessions.forEach(s => {
+		s.size = s.snapshots.length;
+
+		if (!s.size) return;
+
+		s.last = s.snapshots[s.size - 1]['time'];
+
+		s.url = `/tool/a/?id=${s.geography_id}&snapshot=${s.last}`;
+
+		const d = new Date(s.time);
+		s.date = d.toLocaleDateString() + " at " + d.toLocaleTimeString();
+
+		const geos = trees.find(t => t.id === s.geography_id).path.map(e => geographies.find(g => g.id === e).name);
+
+		s.path = geos.join(" › ");
+		s.geography_names_path = geos;
+
+		const m = tmpl('#session');
+		bind(m,s);
+
+		container.append(m);
+	});
+
+	filtering(sessions);
+
+	for (const p of document.querySelectorAll('.bi.bi-pencil'))
+		p.onclick = function() { edit_title.call(this, sessions); };
 };
 
 export async function init() {
@@ -27,6 +110,8 @@ export async function init() {
 		"order":   "time.desc",
 	});
 
+	sessions.forEach(s => s.title = s.title || '-- untitled --');
+
 	const gs = unique(sessions.map(t => t.geography_id));
 
 	const trees = await API.get('geographies_tree_up',   { "id": `in.(${gs.join(',')})`});
@@ -39,25 +124,24 @@ export async function init() {
 
 	const container = document.querySelector('#sessions');
 
-	sessions.forEach(s => {
-		s.size = s.snapshots.length;
+	document.querySelector("#sessions-count").innerText = sessions.length + " Analyses";
 
-		if (!s.size) return;
+	document.querySelector("select").onchange = function() {
+		const v = this.value;
 
-		s.last = s.snapshots[s.size - 1]['time'];
+		const squares = document.getElementsByClassName('session-square');
+		while (squares[0]) squares[0].parentNode.removeChild(squares[0]);
 
-		s.url = `/tool/a/?id=${s.geography_id}&snapshot=${s.last}`;
+		sessions.sort(function(a,b) {
+			if (a[v] > b[v]) return 1;
+			else if (a[v] < b[v]) return -1;
+			else return 0;
+		});
 
-		const d = new Date(s.time);
-		s.date = d.toLocaleDateString() + " at " + d.toLocaleTimeString();
+		draw_sessions(sessions, geographies, container, trees);
+	};
 
-		s.path = trees.find(t => t.id === s.geography_id).path.map(e => geographies.find(g => g.id === e).name).join(" › ");
-
-		const m = tmpl('#session');
-		bind(m,s);
-
-		container.append(m);
-	});
+	draw_sessions(sessions, geographies, container, trees);
 
 	loading(false);
 };

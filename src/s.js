9867aa9fda6aa7a50fef32fe8d7af37a6c8bb48b
Glauber Magalhães <glauber.tecinfo@gmail.com>
2025-02-07 13:53:34 -0300

    remove gtag custom event for country selection and set ID for country element

--- src/s.js
+++ src/s.js
@@ -184,13 +184,13 @@ export async function init() {
 
 	function list(geographies) {
 		for (let co of geographies) {
-			const d = ce('div', ce('h2', co.name, { "class": 'country-name' }), { "class": 'country-item', "ripple": "" });
+			const d = ce('div', ce('h2', co.name, { "class": 'country-name' }), { "id": co.name, "class": 'country-item', "ripple": "" });
 			d.onclick = async _ => {
-				gtag('event', 'button_click', {
-					'event_category': 'Country',
-					'event_label': 'User selects a country',
-					'value': co.name
-				});
+				// gtag('event', 'country', {
+				// 	'event_category': 'Country',
+				// 	'event_label': 'User selects a country',
+				// 	'value': co.name
+				// });
 				preload_boundaries(co.id);
 				setTimeout(_ => geography(co), 350);
 			};

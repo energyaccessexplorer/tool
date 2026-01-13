d5183f42f20902ed38b63eb4c419a32c987be9f6
Macario Ortega <git@aelita.io>
2026-01-13 20:42:19 +0100

    Refactor toast to use template

--- src/components/toast.js
+++ src/components/toast.js
@@ -1,25 +1,16 @@
+import bind from '../../lib/bind.js';
+import { tmpl } from '../../lib/helpers.js';
+
 export default class Toast extends HTMLElement {
 	constructor(options) {
 		super();
 
 		const { label, caption } = options;
 
-		this.innerHTML = `
-			<div class="toast-icon">
-				<i class="bi bi-check-circle-fill"></i>
-			</div>
-			<div class="toast-content">
-				<header class="toast-header">
-					<h1 class="toast-label">${label}</h1>
-					<p class="toast-caption">${caption}</p>
-				</header>
-				<footer class="toast-footer">
-					<div>
-						<slot name="action"></slot>
-					</div>
-				</footer>
-			</div>
-		`;
+		const content = tmpl('#toast-notification-template');
+		bind(content, { label, caption });
+
+		this.append(content);
 	}
 
 	show() {

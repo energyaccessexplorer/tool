e5e6858f8ae56b62ca504ed7f5af7c1ca5254c0b
Macario Ortega <git@aelita.io>
2026-02-17 19:46:02 +0100

    Revert cosmetic changes on src/report.js

--- src/report.js
+++ src/report.js
@@ -13,7 +13,6 @@ import {
 import {
 	analysis_colorscale,
 	analysis_colorscale_svg,
-	lowmedhigh_scale,
 	medhigh_point_count,
 	getpoints as toplocations_fetch,
 } from './analysis.js';
@@ -708,20 +707,20 @@ function analysis_right($, index, rows) {
 	}
 };
 
-async function toplocation_prepare(location) {
+async function toplocation_prepare(t) {
 	const r = {};
 
 	for (const k in SUMMARY)
-		r[k] = SUMMARY[k].raw_raster[location.i];
+		r[k] = SUMMARY[k].raw_raster[t.i];
 
-	const poi = await coords_search_pois({ "coords": location.c, "limit": 1 });
+	const poi = await coords_search_pois({ "coords": t.c, "limit": 1 });
 
 	return Object.assign(
 		r,
-		context(coordinates_to_raster_pixel(location.c))[1],
+		context(coordinates_to_raster_pixel(t.c))[1],
 		{
-			"long": location.c[0],
-			"lat":  location.c[1],
+			"long": t.c[0],
+			"lat":  t.c[1],
 			"poi":  maybe(poi, 0, 'name'),
 		},
 	);
